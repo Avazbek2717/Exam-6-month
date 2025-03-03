59a748c92f33563cbf14660c7bd5539827dbb078
Avazbek <ax1of710@gmail.com>
2025-03-03 13:31:00 +0500

    las

---
+++ apps/common/views.py
@@ -0,0 +1,10 @@
+from .serializers import SponsorCreateSerializer, StudentSponsorCreateSerializer,StudentListSerializer
+from rest_framework.generics import CreateAPIView, ListAPIView
+from . import models
+from rest_framework.views import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.views import APIView
+from django.db.models import Sum
+
+
