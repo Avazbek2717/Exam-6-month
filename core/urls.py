c2b914838cbf60f36c7594e88c10d9bfa4a71059
jasxon <jasurbekx98@gmail.com>
2025-03-06 16:30:22 +0500

    last

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-# from apps.common.views import SponsorCreateAPIView, StudentSponsorCreateAPIView,StudentListAPIView, TotalAmountStatisticAPIView,MonthlyStatisticAPIView
 from .schema import swagger_urlpatterns
 from apps.common.views import NewsCreateView, SendEmail, BannerListAPIView
 
